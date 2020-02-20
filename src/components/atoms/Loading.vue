3d0c00ab32ef1dc3bc6a36c9b5d1a9f19970b3af
清水ハイネ <heine.shimizu@gmail.com>
2020-02-20 20:53:45 +0900

    ローディングアニメーション調整

--- src/components/atoms/Loading.vue
+++ src/components/atoms/Loading.vue
@@ -78,16 +78,16 @@ export default {
     0% {
       transform: translateY(-10px);
     }
-    20% {
-      transform: translateY(-10px);
-    }
-    40% {
-      transform: translateY(0px);
-    }
     60% {
       transform: translateY(-10px);
     }
+    70% {
+      transform: translateY(0px);
+    }
     80% {
+      transform: translateY(-10px);
+    }
+    90% {
       transform: translateY(0px);
     }
     100% {
